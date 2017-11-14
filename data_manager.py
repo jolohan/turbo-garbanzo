3382f773f3536eea19d16e482d46f1162acc566b
johan <johanloudon@gmail.com>
2017-11-14 12:08:15 +0100

    jafj

---
+++ data_manager.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Data_Manager():
+
+    input = np.array()
+    dimension = None
+    input_size = None
+    output_size = None
+
+    def __init__(self, filenumber):
+        filename = (str)(filenumber) + '.txt'
+        temp_matrix = []
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            self.dimension = (int)(lines[2].split(' ')[1])
+            for i in range(5, len(lines)):
+                row = lines[i]
+                if (row == 'EOF'):
+                    break
+                temp_matrix.append(row.split(' ')[1:])
+
+        self.input = np.array(temp_matrix)
+        if (self.dimension != len(temp_matrix)):
+            print("Something wrong with input loading in def __init__ in Data_Manager")
+        self.input_size = self.dimension
+        self.output_size = self.dimension
